883e0c01b9e7c62b038bee533ee3c7900be59f18
Alex Kitching <ak@stowmarketmiddle.suffolk.sch.uk>
2014-01-14 11:44:27 +0000

    Add missing strings to language file

--- lang/en/block_papercutws_quota.php
+++ lang/en/block_papercutws_quota.php
@@ -3,3 +3,14 @@ $string['pluginname'] = 'PaperCut NG WS Quota';
 $string['papercutws_quota'] = 'PaperCut NG WS Quota';
 $string['papercutws_quota:addinstance'] = 'Add a new PaperCut NG WS Quota block';
 $string['papercutws_quota:myaddinstance'] = 'Add a new PaperCut NG WS Quota block to the My Moodle page';
+
+$string['title'] = 'Block Title';
+$string['title_desc'] = 'Please enter the title for the block.';
+$string['authtoken'] = 'Authtoken';
+$string['authtoken_desc'] = 'Please enter the PaperCut server\'s authtoken';
+$string['serveruri'] = 'Server URL';
+$string['serveruri_desc'] = 'Please enter the URL of the PaperCut server.';
+$string['port'] = 'Server Port';
+$string['port_desc'] = 'Which port is the PaperCut server listening on?';
+$string['https'] = 'Use HTTPS';
+$string['https_desc'] = 'Use HTTPS to access the PaperCut Server?';
