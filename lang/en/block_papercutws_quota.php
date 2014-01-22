5ad971a9933596ca0adb02448b7bb6406556adfb
Alex Kitching <ak@stowmarketmiddle.suffolk.sch.uk>
2014-01-22 12:29:40 +0000

    Add language string for icon

--- lang/en/block_papercutws_quota.php
+++ lang/en/block_papercutws_quota.php
@@ -14,3 +14,5 @@ $string['port'] = 'Server Port';
 $string['port_desc'] = 'Which port is the PaperCut server listening on?';
 $string['https'] = 'Use HTTPS';
 $string['https_desc'] = 'Use HTTPS to access the PaperCut Server?';
+
+$string['quota'] = 'Quota remaining';
